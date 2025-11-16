19f347e570392cc546e6d93758e26f8315b8ece8
kalharaweragoda <kalharaweragoda@gmail.com>
2025-11-16 09:03:46 +0530

    controller class add

---
+++ src/main/java/edu/icet/controller/StudentController.java
@@ -0,0 +1,13 @@
+package edu.icet.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+@RequiredArgsConstructor
+public class StudentController {
+
+    final StudentService service;
+}
